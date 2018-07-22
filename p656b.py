971efd2126c2784810ad4b2248630f4cd7878c6e
Makoa <32736507+santarini@users.noreply.github.com>
2018-07-22 11:43:50 -1000

    Update p656b.py

--- p656b.py
+++ p656b.py
@@ -1,20 +1,26 @@
-#! python3
-#graph of r(t) = cos(t)i + sin(t)j + sin(2t)k
-
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
+
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 ax = plt.axes(projection='3d')
 
+#domain
+#zline = np.linspace(0, 15, 1000)
 t=np.linspace(0, 15, 1000)
 
+#equations
 xline = np.cos(t)
 yline = np.sin(t)
 zline = np.sin(2*t)
-ax.plot3D(xline, yline, zline, 'gray')
+ax.plot3D(xline, yline, zline, color='gray')
+
+#axis labels
+ax.set_xlabel('X axis')
+ax.set_ylabel('Y axis')
+ax.set_zlabel('Z axis')
 
 plt.show()
