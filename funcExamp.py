d40161932225c70ad67ac7500e46367fca89ba8d
Makoa <32736507+santarini@users.noreply.github.com>
2018-07-22 12:10:39 -1000

    Create funcExamp.py

---
+++ funcExamp.py
@@ -0,0 +1,35 @@
+from numpy import exp,arange
+from pylab import meshgrid,cm,imshow,contour,clabel,colorbar,axis,title,show
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import matplotlib.pyplot as plt
+
+# the function that I'm going to plot
+def z_func(x,y):
+ return (1-(x**2+y**3))*exp(-(x**2+y**2)/2)
+
+ 
+x = arange(-3.0,3.0,0.1)
+y = arange(-3.0,3.0,0.1)
+X,Y = meshgrid(x, y) # grid of point
+Z = z_func(X, Y) # evaluation of the function on the grid
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, 
+                      cmap=cm.RdBu,linewidth=0, antialiased=False)
+
+ax.zaxis.set_major_locator(LinearLocator(10))
+ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+#axis labels
+ax.set_title('$z=(1-x^2+y^3) e^{-(x^2+y^2)/2}$')
+ax.set_xlabel('X axis')
+ax.set_ylabel('Y axis')
+ax.set_zlabel('Z axis')
+ax.view_init(elev=25, azim=45)
+
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show()
